5f012f5f26da79e26b616e8267e15a809b9d9c1d
Yafeng Wang <yafeng.wang@wyf.local>
2020-11-05 19:17:52 +0800

    格式化Event

--- src/main/java/com/thoughtworks/capability/gtb/vo/EventType.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/EventType.java
@@ -1,11 +1,20 @@
 package com.thoughtworks.capability.gtb.vo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum EventType {
   UPLOAD("U"), DOWNLOAD("D");
 
+  @JsonProperty("type")
   private String code;
 
   EventType(String code) {
     this.code = code;
+  }
+
+  @JsonValue
+  public String getCode() {
+    return this.code;
   }
 }
